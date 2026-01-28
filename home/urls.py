53c38bd2af268a6838318f2ed92731c395f8f769
Jessica Rodriguez Guarayo <JessRod2000@users.noreply.github.com>
2026-01-27 21:31:41 -0400

    Guia1

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views #de inicio . trae a views.py
+
+urlpatterns = [
+    #1 el nombre,2 la vista y luego llamamos a su funcion, 3 
+    path("", views.indice, name="indice"),
+    path("pato",views.pato, name="patito"), #para acceder aqui es /home/pato
+    path("vaca",views.vaca, name='vaquita')
+]
